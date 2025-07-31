c362536d0f303e5a10d2deb72bb7971eaa3fd7ab
Glen Parker <v-glenparker@microsoft.com>
2025-07-31 15:12:40 -0700

    Error, Fatal On crash, flash each of eight bits of the error code on the LED, with short flash meaning zero, and long flash meaning one. Shift all the error codes down by one.

--- ArduinoPowerMonitor/Abort.c
+++ ArduinoPowerMonitor/Abort.c
@@ -10,17 +10,28 @@ void fatal(uint8_t code)
     
     for (;;)
     {
-        for (uint8_t j = 0; j < code; ++j)
+        for (int8_t shift = 7; shift >= 0; --shift)
         {
+            uint32_t bitDelay;
+
+            if ((code >> shift) & 0x00000001)
+            {
+                bitDelay = 800000;
+            }
+            else
+            {
+                bitDelay = 300000;
+            }
+
             led_on();
 
-            for (uint32_t i = 0; i < 200000; ++i);
-        
+            for (uint32_t i = 0; i < bitDelay; ++i);
+            
             led_off();
 
             for (uint32_t i = 0; i < 200000; ++i);
-        }            
+        }
 
-        for (uint32_t i = 0; i < 800000; ++i);
+        for (uint32_t i = 0; i < 2000000; ++i);
     }
 }
